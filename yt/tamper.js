95c2b027cd051bf661d2b69f744a5c032c1bf6fc
Iker Garitaonandia <orloxx@users.noreply.github.com>
2022-07-26 10:37:20 +0200

    Update tamper.js

--- yt/tamper.js
+++ yt/tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube: automated AYA
 // @namespace    https://iker.io
-// @version      0.4
+// @version      0.5
 // @description  YouTube Music Player performs an are-you-alive check sometimes. This will click "Yes" always.
 // @author       iker.io
 // @match        https://music.youtube.com/*
@@ -14,15 +14,12 @@
 
   const $parent = document.querySelector('ytmusic-popup-container');
   const observer = new MutationObserver(() => {
-    const $areYouThereModal = document.querySelector('ytmusic-you-there-renderer');
-    if ($areYouThereModal) {
-      const yesButton = $areYouThereModal.querySelector('yt-button-renderer');
-
-      requestAnimationFrame(() => {
-        if (yesButton && yesButton.clientHeight > 0) {
-          yesButton.click();
-        }
-      });
+    try {
+      const $areYouThereModal = $parent.querySelector('ytmusic-you-there-renderer');
+      $areYouThereModal.querySelector('yt-button-renderer').click();
+    } catch(error) {
+      // do nothing
+      console.error('Oopsies: ', error);
     }
   });
 
